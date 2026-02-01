4df2fec7b04adb0b07769f2a3a45efb7decc48c2
Chloe Haynes <chloehaynes5252@gmail.com>
2026-01-31 22:17:39 -0700

    Initial commit

---
+++ src/main/java/net/chloe/paintmod/Paintmod.java
@@ -0,0 +1,15 @@
+package net.chloe.paintmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Paintmod implements ModInitializer {
+	public static final String MOD_ID = "paintmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/chloe/paintmod/PaintmodClient.java
@@ -0,0 +1,11 @@
+package net.chloe.paintmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaintmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
