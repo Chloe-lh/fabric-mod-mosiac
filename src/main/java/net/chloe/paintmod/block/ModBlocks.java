7b0ffbd173fdacd593d9341b551d4b30b3bb1a8d
Chloe Haynes <chloehaynes5252@gmail.com>
2026-02-10 00:52:26 -0700

    UPDATE, update java/fabric ver, running

--- src/main/java/net/chloe/paintmod/block/ModBlocks.java
+++ src/main/java/net/chloe/paintmod/block/ModBlocks.java
@@ -2,39 +2,50 @@ package net.chloe.paintmod.block;
 import net.chloe.paintmod.Paintmod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import java.util.function.Function;
 
 public class ModBlocks {
 
     public static final Block CERAMIC_BLOCK = registerBlock("ceramic_block",
-        new Block(AbstractBlock.Settings.create()
-                .strength(2f)
-                .sounds(BlockSoundGroup.GLASS)));
-    // need loot table in order to drop resources when broken
+            properties -> new Block(properties
+                    .strength(4f)
+                    .sounds(BlockSoundGroup.GLASS)));
 
-    private static Block registerBlock(String name, Block block){
-        Block registeredBlock = Registry.register(Registries.BLOCK, Identifier.of(Paintmod.MOD_ID, name), block);
-        registerBlockItem(name, registeredBlock);
-        return registeredBlock;
+
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> function){
+        Block registering = function.apply(AbstractBlock.Settings.create()
+                .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Paintmod.MOD_ID, name))));
+        registerBlockItem(name, registering);
+        return Registry.register(Registries.BLOCK, Identifier.of(Paintmod.MOD_ID, name), registering);
     }
-    // register block as an item
-    private static void registerBlockItem(String name, Block block){
+
+    private static Block registerBlockWithoutBlockItem(String name, Function<AbstractBlock.Settings, Block> function) {
+        return Registry.register(Registries.BLOCK, Identifier.of(Paintmod.MOD_ID, name),
+                function.apply(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Paintmod.MOD_ID, name)))));
+    }
+
+    private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(Paintmod.MOD_ID, name),
-                new BlockItem(block, new Item.Settings()));
+                new BlockItem(block, new Item.Settings().useBlockPrefixedTranslationKey()
+                        .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Paintmod.MOD_ID, name)))));
     }
-    // for onInitializer
-    public static void registerModBlocks(){
-        Paintmod.LOGGER.info("Registering Mod blocks for "+ Paintmod.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries->{
-            entries.add(ModBlocks.CERAMIC_BLOCK);
-        });
+
+    public static void registerModBlocks() {
+        Paintmod.LOGGER.info("Registering Mod blocks for " + Paintmod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS)
+                .register(entries -> entries.add(CERAMIC_BLOCK));
     }
 }
+
---
+++ src/main/java/net/chloe/paintmod/item/ModItem.java
@@ -0,0 +1,9 @@
+package net.chloe.paintmod.item;
+
+import net.chloe.paintmod.Paintmod;
+
+public class ModItem {
+    public static void registerModItems(){
+        Paintmod.LOGGER.info("Registering mod items for "+ Paintmod.MOD_ID);
+    }
+}
